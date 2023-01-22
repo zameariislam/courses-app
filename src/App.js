25eb0fd4300f61bfd7c91e1bc899d7332d2815d3
zameariislam <zameariislam@gmail.com>
2023-01-22 15:21:22 +0100

    firebase initiated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
-
+ import Header from "./components/Header/Header";
 function App() {
   return (
     <div>
-      hello
+      <Header></Header>
+
       
+    
+
 
     </div>
   );
--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -11,7 +11,7 @@ const firebaseInitialization = () => {
 
 };
 
-export default firebaseInitialization
+export default firebaseInitialization 
 
 
 
