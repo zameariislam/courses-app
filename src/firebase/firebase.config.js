0285d7c79d9bfd52c321e7a80cc39aaf64e4c82f
zameariislam <zameariislam@gmail.com>
2023-01-22 14:53:22 +0100

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,12 @@
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDFc1027k6EBxvqw-WAiTtibfQ2Lj8EH_Y",
+    authDomain: "courses-app-1430a.firebaseapp.com",
+    projectId: "courses-app-1430a",
+    storageBucket: "courses-app-1430a.appspot.com",
+    messagingSenderId: "1037237165864",
+    appId: "1:1037237165864:web:3ac896f65d661fad76ba1d"
+  };
+  
+  export default firebaseConfig
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase.config";
+
+
+
+
+const firebaseInitialization = () => {
+
+    return initializeApp(firebaseConfig)
+
+};
+
+export default firebaseInitialization
+
+
+
+
